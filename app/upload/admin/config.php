2c3fd9243e869991ef4f38f2cbfd3b52a8738648
Lucas <lucas@Silver-Surfer.local>
2019-04-05 00:39:28 -0700

    Containerization working.

--- app/upload/admin/config.php
+++ app/upload/admin/config.php
@@ -1,42 +1,42 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/quickcommerce-vestracing/upload/admin/');
-define('HTTP_CATALOG', 'http://localhost/quickcommerce-vestracing/upload/');
+define('HTTP_SERVER', 'http://localhost:8081/admin/');
+define('HTTP_CATALOG', 'http://localhost:8081/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/quickcommerce-vestracing/upload/admin/');
-define('HTTPS_CATALOG', 'http://localhost/quickcommerc-vestracing/upload/');
+define('HTTPS_SERVER', 'http://localhost:8081/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8081/');
 
 // DIR
-define('DIR_APPLICATION', '/Web/quickcommerce-vestracing/upload/admin/');
-define('DIR_SYSTEM', '/Web/quickcommerce-vestracing/upload/system/');
-define('DIR_LANGUAGE', '/Web/quickcommerce-vestracing/upload/admin/language/');
-define('DIR_TEMPLATE', '/Web/quickcommerce-vestracing/upload/admin/view/template/');
-define('DIR_CONFIG', '/Web/quickcommerce-vestracing/upload/system/config/');
-define('DIR_IMAGE', '/Web/quickcommerce-vestracing/upload/image/');
-define('DIR_CACHE', '/Web/quickcommerce-vestracing/upload/system/storage/cache/');
-define('DIR_DOWNLOAD', '/Web/quickcommerce-vestracing/upload/system/storage/download/');
-define('DIR_UPLOAD', '/Web/quickcommerce-vestracing/upload/system/storage/upload/');
-define('DIR_LOGS', '/Web/quickcommerce-vestracing/upload/system/storage/logs/');
-define('DIR_MODIFICATION', '/Web/quickcommerce-vestracing/upload/system/storage/modification/');
-define('DIR_CATALOG', '/Web/quickcommerce-vestracing/upload/catalog/');
-define('DIR_QC', '/Web/quickcommerce-vestracing/');
+define('DIR_APPLICATION', '/var/www/upload/admin/');
+define('DIR_SYSTEM', '/var/www/upload/system/');
+define('DIR_LANGUAGE', '/var/www/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/upload/system/config/');
+define('DIR_IMAGE', '/var/www/upload/image/');
+define('DIR_CACHE', '/var/www/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/upload/system/storage/download/');
+define('DIR_UPLOAD', '/var/www/upload/system/storage/upload/');
+define('DIR_LOGS', '/var/www/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/upload/system/storage/modification/');
+define('DIR_CATALOG', '/var/www/upload/catalog/');
+define('DIR_QC', '/var/www/');
 define('QUICKCOMMERCE_INSTALLED', true);
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'mariadb');
+define('DB_USERNAME', 'vestracing');
+define('DB_PASSWORD', 'quickcommerce');
 define('DB_DATABASE', 'vest_quickcommerce');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc2_');
 
 // DB2
 define('DB2_DRIVER', 'mysqli');
-define('DB2_HOSTNAME', 'localhost');
+define('DB2_HOSTNAME', 'mariadb');
 define('DB2_USERNAME', 'root');
-define('DB2_PASSWORD', '');
+define('DB2_PASSWORD', 'quickcommerce');
 define('DB2_DATABASE', 'vest_quickcommerce');
 define('DB2_PORT', '3306');
 define('DB2_PREFIX', 'oc2_');
--- app/upload/config.php
+++ app/upload/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/quickcommerce-vestracing/upload/');
+define('HTTP_SERVER', 'http://localhost:8081/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/quickcommerce-vestracing/upload/');
+define('HTTPS_SERVER', 'http://localhost:8081/');
 
 // DIR
-define('DIR_APPLICATION', '/Web/quickcommerce-vestracing/upload/catalog/');
-define('DIR_SYSTEM', '/Web/quickcommerce-vestracing/upload/system/');
-define('DIR_LANGUAGE', '/Web/quickcommerce-vestracing/upload/catalog/language/');
-define('DIR_TEMPLATE', '/Web/quickcommerce-vestracing/upload/catalog/view/theme/');
-define('DIR_CONFIG', '/Web/quickcommerce-vestracing/upload/system/config/');
-define('DIR_IMAGE', '/Web/quickcommerce-vestracing/upload/image/');
-define('DIR_CACHE', '/Web/quickcommerce-vestracing/upload/system/cache/');
-define('DIR_DOWNLOAD', '/Web/quickcommerce-vestracing/upload/system/download/');
-define('DIR_UPLOAD', '/Web/quickcommerce-vestracing/upload/system/upload/');
-define('DIR_MODIFICATION', '/Web/quickcommerce-vestracing/upload/system/modification/');
-define('DIR_LOGS', '/Web/quickcommerce-vestracing/upload/system/logs/');
-define('DIR_QC', '/Web/quickcommerce-vestracing/');
+define('DIR_APPLICATION', '/var/www/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/upload/system/');
+define('DIR_LANGUAGE', '/var/www/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/upload/system/config/');
+define('DIR_IMAGE', '/var/www/upload/image/');
+define('DIR_CACHE', '/var/www/upload/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/upload/system/download/');
+define('DIR_UPLOAD', '/var/www/upload/system/upload/');
+define('DIR_MODIFICATION', '/var/www/upload/system/modification/');
+define('DIR_LOGS', '/var/www/upload/system/logs/');
+define('DIR_QC', '/var/www/');
 define('QUICKCOMMERCE_INSTALLED', true);
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'mariadb');
+define('DB_USERNAME', 'vestracing');
+define('DB_PASSWORD', 'quickcommerce');
 define('DB_DATABASE', 'vest_quickcommerce');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc2_');
